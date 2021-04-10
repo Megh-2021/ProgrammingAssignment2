2aaa9996cd1b14df86fec1cfd737f74e427c5020
Megh-2021 <meghiitr@gmail.com>
2021-04-10 15:14:52 +0530

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,16 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv<-NULL
+  set<- function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function(){x}
+  setInverse <- function(inverse){inv<<-inverse}
+  getInverse <-function(){inv}
+  list(set =set, get= get, setInverse = setInverse, getInverse = getInverse)
+
 }
 
 
@@ -12,4 +22,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv<- x$getInverse()
+  if(!is.null(inv)){
+    message("Retrieving cached data")
+    return(inv)
+  }
+  mat <-x$get()
+  inv <-solv(mat, ...)
+  x$setInverse(inv)
+  inv
+}
+hilbert<- function(n){
+  i<- 1:n
+  1/outer(i-1,i,"+")
+  
 }
